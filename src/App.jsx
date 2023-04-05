84f2b7995b931cc3dcbdd63396e8bb6ad87b734d
Olof Sjögren <ropaolle@gmail.com>
2023-04-05 20:16:56 +0200

    Feat/dude (#43)
    * preview deploy
    
    * needs build
    
    * rename cf project
    
    * hk
    
    * hk

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,12 @@ function App() {
       <h3>{process.env.CF_PAGES_BRANCH}</h3>
       <h3>{process.env.CF_PAGES_URL}</h3>
       <h3>{process.env.CUSTOM_VAR}</h3>
+      <h3>{import.meta.env.VITE_SOME_KEY}</h3>
+      <h3>{import.meta.env.VITE_SOME_KEY2}</h3>
+      <h3>{import.meta.env.VITE_SOME_KEY3}</h3>
+      <h3>{import.meta.env.VITE_SOME_KEY4}</h3>
+      <h3>{import.meta.env.VITE_CUSTOM_VAR}</h3>
+      <h3>{import.meta.env.VITE_CUSTOM_VAR2}</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
