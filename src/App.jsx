4d8b1594d3357f3cd7ecdbb3baeb2410aef44a4f
ropaolle <ropaolle@gmail.com>
2023-04-05 21:11:03 +0200

    .env

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { useState } from "react"
+import reactLogo from "./assets/react.svg"
 // eslint-disable-next-line import/no-unresolved
-import viteLogo from "/vite.svg";
-import "./App.css";
+import viteLogo from "/vite.svg"
+import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
   return (
     <div className="App">
       <div>
@@ -17,15 +17,10 @@ function App() {
         </a>
       </div>
       <h1>Vite + React + GitHub Actions</h1>
-      <h3>{process.env.CF_PAGES_BRANCH}</h3>
-      <h3>{process.env.CF_PAGES_URL}</h3>
-      <h3>{process.env.CUSTOM_VAR}</h3>
-      <h3>{import.meta.env.VITE_SOME_KEY}</h3>
-      <h3>{import.meta.env.VITE_SOME_KEY2}</h3>
-      <h3>{import.meta.env.VITE_SOME_KEY3}</h3>
-      <h3>{import.meta.env.VITE_SOME_KEY4}</h3>
-      <h3>{import.meta.env.VITE_CUSTOM_VAR}</h3>
-      <h3>{import.meta.env.VITE_CUSTOM_VAR2}</h3>
+      <h3>OLLE_TEST_KEY: {process.env.OLLE_TEST_KEY}</h3>
+      <h3>VITE_OLLE_TEST_KEY: {process.env.VITE_OLLE_TEST_KEY}</h3>
+      <h3>VITE_OLLE_TEST_KEY2: {import.meta.env.VITE_OLLE_TEST_KEY2}</h3>
+      <h3>VITE_TEST_KEY: {import.meta.env.VITE_TEST_KEY}</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -38,7 +33,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
