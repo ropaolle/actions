15500902e4eff9b1a9fb4febb258796c28219ac6
ropaolle <ropaolle@gmail.com>
2023-04-06 15:05:48 +0200

    hk

--- .github/actions/docker/entrypoint.sh
+++ .github/actions/docker/entrypoint.sh
@@ -3,3 +3,7 @@
 echo "Hello $1"
 time=$(date)
 echo "time=$time" >>$GITHUB_OUTPUT
+
+# Run below to make the file executable
+# git add .github/actions/docker/entrypoint.sh
+# olle@SOVRUM:~/GITHOME/actions$ git update-index --chmod=+x .github/actions/docker/entrypoint.sh
